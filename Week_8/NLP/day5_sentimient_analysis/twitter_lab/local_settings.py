42e2fcd87e29dd0e066ac8d779d641d5161f66c2
patrickrhbrown <patrickrhbrown@me.com>
2017-08-18 14:40:00 +0100

    2.0

---
+++ Week_8/NLP/day5_sentimient_analysis/twitter_lab/local_settings.py
@@ -0,0 +1,6 @@
+twitter_keys = {
+    'consumer_key':        'u9AI6IvPUESTVoE6Hfk048Kzc',
+    'consumer_secret':     'CFgnKuUSM8jg2xMbP9xItsoNBCe8dBt8Wzn8FAKENXeBKVTJtY',
+    'access_token_key':    '857885778373955584-1Bj41ua8ywCMxKxpfStc4WeHteSXGPP',
+    'access_token_secret': 'kQGh7IhA5Ab71RNKZhS6xIQ5N3K9MZ9EPHRlKeAaY0c7d'
+}
